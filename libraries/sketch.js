35468d244a4a80e5ca594124d2198ab811318562
Darren Zhang <darrenzhang200027@gmail.com>
2020-04-06 00:49:47 -0400

    added logic for deleting pipes after leaving screen

--- libraries/sketch.js
+++ libraries/sketch.js
@@ -1,13 +1,28 @@
 var bird
+var pipes = []
 function setup() {
     createCanvas(400, 600)
     bird = new Bird()
+    pipes.push(new Pipe())
+    frameRate(60)
 }
 
 function draw() {
     background(0)
     bird.show()
     bird.update()
+
+    if (frameCount % 100 == 0){
+        pipes.push(new Pipe())
+    }
+
+    for (var i = pipes.length - 1; i >= 0; i--){
+        pipes[i].show()
+        pipes[i].update()
+        if (pipes[i].offscreen()){
+            pipes.splice(i, 1)
+        }
+    }
 }
 
 function keyPressed() {
