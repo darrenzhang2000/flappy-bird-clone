88a79cffae15bf18400d781d50740e8081990bf1
Darren Zhang <darrenzhang200027@gmail.com>
2020-04-06 00:19:53 -0400

    added bird and canvas

---
+++ libraries/bird.js
@@ -0,0 +1,27 @@
+function Bird() {
+    this.y = width / 2
+    this.x = 64
+
+    this.gravity = 1
+    this.velocity = 0
+
+    this.show = function () {
+        fill(255) //set color to white
+        ellipse(this.x, this.y, 32, 32) //draw white ellipse
+    }
+
+    this.update = function () {
+        this.velocity += this.gravity
+        this.y += this.velocity
+    
+        if (this.y > height){
+            this.y = height
+            this.velocity = 0
+        }
+
+        if (this.y < 0){
+            this.y = 0
+            this.velocity = 0
+        }
+    }
+}
