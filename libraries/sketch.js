d088d3b54dc1e72c5c4dbfc05eded9a9db99a037
Darren Zhang <darrenzhang200027@gmail.com>
2020-04-06 00:58:37 -0400

    added highlighting upon collision

--- libraries/sketch.js
+++ libraries/sketch.js
@@ -9,19 +9,27 @@ function setup() {
 
 function draw() {
     background(0)
+
+    for (var i = pipes.length - 1; i >= 0; i--){
+        pipes[i].show()
+        pipes[i].update()
+
+        if(pipes[i].hits(bird)){
+            console.log("HIT")
+        }
+
+        if (pipes[i].offscreen()){
+            pipes.splice(i, 1)
+        }
+    }
+
+
+
     bird.show()
     bird.update()
 
     if (frameCount % 100 == 0){
         pipes.push(new Pipe())
-    }
-
-    for (var i = pipes.length - 1; i >= 0; i--){
-        pipes[i].show()
-        pipes[i].update()
-        if (pipes[i].offscreen()){
-            pipes.splice(i, 1)
-        }
     }
 }
 
